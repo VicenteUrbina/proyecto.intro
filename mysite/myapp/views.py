49055d8c2787ece809825aa0ce21ca15809b1ddd
VicenteUrbina <vicenteurbinahd@gmail.com>
2024-10-11 17:06:39 -0300

    Proyecto con la base creada y el html vinculado

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+def myapp(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
