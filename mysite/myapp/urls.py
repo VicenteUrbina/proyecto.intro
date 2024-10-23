6e6bc9a8437524d1166e86c216dd30fcf7d64829
BenjaminLira17 <benjaminliraespinoza@gmail.com>
2024-10-22 21:23:22 -0300

    creacion carpeta static, cambio de html
    La carpeta static contiene el style.css, el cual ya esta connectado con los archivos de html.

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path('', views.home, name='myapp'),
-    path('myapp/', views.singup, name='myapp')
+    path('myapp/', views.signup, name='myapp')
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+
 def home(request):
     return render(request,'home.html')
-def singup(request):
+def signup(request):
     return render(request,'index.html')
